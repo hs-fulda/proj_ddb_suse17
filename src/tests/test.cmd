8bb300834f6c3499e147b063039884eb0b3b60cd
aprobinda <aprobinda@gmail.com>
2017-08-24 17:03:52 +0200

    manual test works again. Usage: 1) src/parser/runjavacc.cmd 2) go to tests folder and open a cmd window from there. Type "test testCaseName".

--- src/tests/test.cmd
+++ src/tests/test.cmd
@@ -1,5 +1,5 @@
 @echo off
-REM Example of Usage: test.cmd _v_ListOfAttributes.txt
+REM Example of Usage: test.cmd ListOfAttributes.txt
 REM Do not change below this line
-javac *.java
-java -cp .. tests.GepardParser<%1
+REM javac *.java
+java -cp .. parser.GepardParser<%1
