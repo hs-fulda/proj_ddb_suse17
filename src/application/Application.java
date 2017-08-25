25f038bf331fb56c2a5d8f7a61acad455e47b63d
Aleksandr Anfilov <aprobinda@gmail.com>
2017-08-25 15:44:19 +0200

    Added: - closing of JDBC connections (after exceptions also); - formatting style XML file for Eclipse; - log that connections are closed.

--- src/application/Application.java
+++ src/application/Application.java
@@ -11,83 +11,90 @@ import fjdbc.FedStatement;
 import parser.ParseException;
 
 public class Application {
+  private static long startTime;
+  private static long duration;
 
-	private static long startTime;
-	private static long duration;
+  public static void main(String[] args) throws ParseException, FedException {
+    // Selects files and stores scripts in a list
+    File selectedFile = FileUtility.getFile();
+    List<String> scriptsFromSelectedFile = FileUtility
+	.getScriptsFromFile(selectedFile);
+    FedConnection fedConnection = null;
+    try {
 
-	public static void main(String[] args) throws ParseException {
-		// Selects files and stores scripts in a list
-		File selectedFile = FileUtility.getFile();
-		List<String> scriptsFromSelectedFile = FileUtility.getScriptsFromFile(selectedFile);
+      // Gets connection based on Username and Password
+      fedConnection = new FedPseudoDriver().getConnection(
+	  ApplicationConstants.USERNAME, ApplicationConstants.PASSWORD);
+      fedConnection.setAutoCommit(false);
 
-		try {
+      FedStatement fedStatement = fedConnection.getStatement();
 
-			// Gets connection based on Username and Password
-			FedConnection fedConnection = new FedPseudoDriver().getConnection(ApplicationConstants.USERNAME,
-					ApplicationConstants.PASSWORD);
-			fedConnection.setAutoCommit(false);
+      // Just a formatting to display result
+      OutputFormatter.printAstericks();
+      System.out.println("Executing script file \'"
+	  + selectedFile.getAbsolutePath() + "\' ...");
 
-			FedStatement fedStatement = fedConnection.getStatement();
+      // Time starts
+      startTime = System.currentTimeMillis();
 
-			// Just a formatting to display result
-			OutputFormatter.printAstericks();
-			System.out.println("Executing script file \'" + selectedFile.getAbsolutePath() + "\' ...");
+      // All scripts taken from file provided run here one by one
+      int totalOperations = 0;
+      for (String currentScript : scriptsFromSelectedFile) {
+	// If the query is DDL or DML, executeUpdate should be called
+	// from FJDBC
+	if (isDDLOrDMLScript(currentScript.toUpperCase()))
+	  fedStatement.executeUpdate(currentScript);
+	else if (isCommit(currentScript.toUpperCase()))
+	  fedConnection.commit();
+	else if (isRollback(currentScript.toUpperCase()))
+	  fedConnection.rollback();
+	else
+	  fedStatement.executeQuery(currentScript);
 
-			// Time starts
-			startTime = System.currentTimeMillis();
+	totalOperations++;
+      }
 
-			// All scripts taken from file provided run here one by one
-			int totalOperations = 0;
-			for (String currentScript : scriptsFromSelectedFile) {
-				// If the query is DDL or DML, executeUpdate should be called
-				// from FJDBC
-				if (isDDLOrDMLScript(currentScript.toUpperCase()))
-					fedStatement.executeUpdate(currentScript);
-				else if (isCommit(currentScript.toUpperCase()))
-					fedConnection.commit();
-				else if (isRollback(currentScript.toUpperCase()))
-					fedConnection.rollback();
-				else
-					fedStatement.executeQuery(currentScript);
+      System.out.println(totalOperations + " operations");
 
-				totalOperations++;
-			}
+      // Prints Time Taken
+      System.out.println(getTimeTaken());
+      OutputFormatter.printAstericks();
+    } catch (FedException e) {
+      System.out.println(e);
+    }
+    // close all JDBC connections
+    finally {
+      fedConnection.close();
+    }
 
-			System.out.println(totalOperations + " operations");
+  }
 
-			// Prints Time Taken
-			System.out.println(getTimeTaken());
-			OutputFormatter.printAstericks();
-		} catch (FedException e) {
-			System.out.println(e);
-		}
+  private static boolean isRollback(String script) {
+    return script.startsWith("ROLLBACK");
+  }
 
-	}
+  private static boolean isCommit(String script) {
+    return script.startsWith("COMMIT");
+  }
 
-	private static boolean isRollback(String script) {
-		return script.startsWith("ROLLBACK");
-	}
+  /**
+   * @param script
+   * @return
+   */
+  private static boolean isDDLOrDMLScript(String script) {
+    return script.startsWith("CREATE") || script.startsWith("DROP")
+	|| script.startsWith("INSERT") || script.startsWith("DELETE")
+	|| script.startsWith("UPDATE") || script.startsWith("ALTER")
+	|| script.startsWith("SET");
+  }
 
-	private static boolean isCommit(String script) {
-		return script.startsWith("COMMIT");
-	}
-
-	/**
-	 * @param script
-	 * @return
-	 */
-	private static boolean isDDLOrDMLScript(String script) {
-		return script.startsWith("CREATE") || script.startsWith("DROP") || script.startsWith("INSERT")
-				|| script.startsWith("DELETE") || script.startsWith("UPDATE") || script.startsWith("ALTER")
-				|| script.startsWith("SET");
-	}
-
-	private static String getTimeTaken() {
-		duration = System.currentTimeMillis() - startTime;
-		Date timeTaken = new Date(duration);
-		String timeTakenStr = String.format("Time Taken : %2d Min : %2d Sec : %3d Millis", timeTaken.getMinutes(),
-				timeTaken.getSeconds(), (timeTaken.getTime() % 1000));
-		return timeTakenStr;
-	}
+  private static String getTimeTaken() {
+    duration = System.currentTimeMillis() - startTime;
+    Date timeTaken = new Date(duration);
+    String timeTakenStr = String.format(
+	"Time Taken : %2d Min : %2d Sec : %3d Millis", timeTaken.getMinutes(),
+	timeTaken.getSeconds(), (timeTaken.getTime() % 1000));
+    return timeTakenStr;
+  }
 
 }
