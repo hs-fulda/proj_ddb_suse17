3e3d99ebdaa826f14657380fa5dc16730019edc4
Aleksandr Anfilov <aprobinda@gmail.com>
2017-08-28 20:10:56 +0200

    Now FileUtility returns all statements in UPPERCASE. Added updated scripts from professor.

--- src/application/FileUtility.java
+++ src/application/FileUtility.java
@@ -17,99 +17,105 @@ import javax.swing.filechooser.FileSystemView;
 
 public class FileUtility {
 
-	public static File getFile() {
-		File scriptFile = null;
+  public static File getFile() {
+    File sqlFile = null;
 
-		JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-		jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
-		FileFilter sql = new FileNameExtensionFilter("SQL Dump File (.sql)", "sql");
-		jfc.addChoosableFileFilter(sql);
+    JFileChooser jfc = new JFileChooser(
+	FileSystemView.getFileSystemView().getHomeDirectory());
+    jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+    FileFilter sql = new FileNameExtensionFilter("SQL Dump File (.sql)", "sql");
+    jfc.addChoosableFileFilter(sql);
 
-		int returnValue = jfc.showOpenDialog(null);
+    int returnValue = jfc.showOpenDialog(null);
 
-		String message = "Only SQL Dump (.sql) file is accepted.\nDo you want to try again!";
-		if (returnValue == JFileChooser.APPROVE_OPTION) {
-			scriptFile = jfc.getSelectedFile();
-			if (!(scriptFile != null && scriptFile.getAbsolutePath().toLowerCase().endsWith(".sql"))) {
-				int selectedOption = JOptionPane.showConfirmDialog(null, message, "Choose", JOptionPane.YES_NO_OPTION);
-				if (selectedOption == JOptionPane.YES_OPTION) {
-					getFile();
-				} else {
-					System.exit(1);
-				}
-			}
-		} else {
-			int selectedOption = JOptionPane.showConfirmDialog(null, message, "Choose", JOptionPane.YES_NO_OPTION);
-			if (selectedOption == JOptionPane.YES_OPTION) {
-				scriptFile = getFile();
-			} else {
-				System.exit(1);
-			}
-		}
-
-		return scriptFile;
+    String message = "Only SQL Dump (.sql) file is accepted.\nDo you want to try again?";
+    if (returnValue == JFileChooser.APPROVE_OPTION) {
+      sqlFile = jfc.getSelectedFile();
+      if (!(sqlFile != null
+	  && sqlFile.getAbsolutePath().toLowerCase().endsWith(".sql"))) {
+	int selectedOption = JOptionPane.showConfirmDialog(null, message,
+	    "Choose", JOptionPane.YES_NO_OPTION);
+	if (selectedOption == JOptionPane.YES_OPTION) {
+	  getFile();
+	} else {
+	  System.exit(1);
 	}
+      }
+    } else {
+      int selectedOption = JOptionPane.showConfirmDialog(null, message,
+	  "Choose", JOptionPane.YES_NO_OPTION);
+      if (selectedOption == JOptionPane.YES_OPTION) {
+	sqlFile = getFile();
+      } else {
+	System.exit(1);
+      }
+    }
 
-	public static List<String> getScriptsFromFile(File file) {
-		List<String> scripts = new ArrayList<String>();
+    return sqlFile;
+  }
 
-		BufferedReader br = null;
-		try {
-			StringBuilder script = new StringBuilder();
-			String line;
+  public static List<String> getStatementsFromFile(File file) {
+    List<String> sQLStatements = new ArrayList<String>();
 
-			br = new BufferedReader(new FileReader(file));
+    BufferedReader br = null;
+    try {
+      StringBuilder script = new StringBuilder();
+      String line;
 
-			while ((line = br.readLine()) != null) {
-				line = fetchValidScript(line);
+      br = new BufferedReader(new FileReader(file));
 
-				if (line.trim().isEmpty())
-					continue;
+      while ((line = br.readLine()) != null) {
+	line = fetchValidLine(line);
 
-				if (line.contains(";")) {
-					String[] strArr = line.split(";");
-					for (String str : strArr) {
-						if (str.isEmpty())
-							continue;
+	if (line.trim().isEmpty())
+	  continue;
 
-						script.append(str);
-					}
+	if (line.contains(";")) {
+	  String[] strArr = line.split(";");
+	  for (String str : strArr) {
+	    if (str.isEmpty())
+	      continue;
 
-					if (script.length() > 0) {
-						scripts.add(script.toString());
-					}
-					script = new StringBuilder();
-				} else {
-					script.append(line);
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (br != null)
-					br.close();
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
-		}
+	    script.append(str);
+	  }
 
-		return scripts;
+	  if (script.length() > 0) {
+	    sQLStatements.add(script.toString());
+	  }
+	  script = new StringBuilder();
+	} else {
+	  script.append(line);
 	}
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+	if (br != null)
+	  br.close();
+      } catch (IOException ex) {
+	ex.printStackTrace();
+      }
+    }
 
-	// Does not consider comments (e.g. -- // /* \\ # as scripts
-	private static String fetchValidScript(String script) {
-		StringBuilder validScript = new StringBuilder();
-		Scanner input = new Scanner(script.toString());
-		while (input.hasNextLine()) {
-			String line = input.nextLine();
-			if (line.startsWith("--") || line.startsWith("\\\\") || line.startsWith("//") || line.startsWith("/*")
-					|| line.startsWith("#") || line.trim().isEmpty())
-				continue;
-			else
-				validScript.append(line);
-		}
-		return validScript.toString();
-	}
+    return sQLStatements;
+  }
+
+  // Does not consider comments (e.g. -- // /* \\ # as scripts
+  private static String fetchValidLine(String script) {
+    StringBuilder validLine = new StringBuilder();
+    Scanner input = new Scanner(script.toString());
+    while (input.hasNextLine()) {
+      String line = input.nextLine();
+      if (line.startsWith("--") || line.startsWith("\\\\")
+	  || line.startsWith("//") || line.startsWith("/*")
+	  || line.startsWith("#") || line.trim().isEmpty())
+	continue;
+      else {
+	validLine.append(line.toUpperCase());
+      }
+    }
+    return validLine.toString();
+  }
 
 }
